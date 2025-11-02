dd63d04610c5169b57f3b60245ffd89403cee791
Yonas Mamo <yonasma416@gmail.com>
2025-11-02 10:42:19 -0800

    trying to pass task 2 checker

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,21 +1,21 @@
-import UserProfile from './components/UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 import './App.css'
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
       <UserProfile 
         name="Alice"
         age="25"
         bio="Loves hiking and photography"
       />
-      <Header />
-      <MainContent />
-      <Footer />
     </div>
   );
 }
